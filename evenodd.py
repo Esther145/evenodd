4d189ae9355361dc7b09530eb7f4b5fd8b1c8e2a
Esther145 <esther.thomson98@gmail.com>
2020-04-28 23:35:32 +0400

    Even or Odd

---
+++ evenodd.py
@@ -0,0 +1,3 @@
+import evod as e
+a=int(input("\nEnter the number you want to check if it is even or odd\n "))
+e.evodd(a)
---
+++ evod.py
@@ -0,0 +1,7 @@
+def evodd(x):
+    if(x%2==0):
+        print(" The Number is Even ")
+    else:
+        print(" The Number is Odd ")
+
+    
